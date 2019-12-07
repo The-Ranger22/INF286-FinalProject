364ce2931765f9af05bd2345d4a7b71e231d61a5
TheRanger22 <schandingl1@nku.edu>
2019-12-07 12:24:33 -0500

    Tinkering with the blog post scripts

--- js/community.js
+++ js/community.js
@@ -1,9 +1,31 @@
+console.log("Page loaded");
 
-class BlogPost{
+class Post{
     constructor(title, date, content){
         this.title = title;
         this.date = date;
         this.content = content;
     }
 }
+
+
+
+let blogMap = new Map();
+$.getJSON("https://api.myjson.com/bins/nnlio",function(data){
+   for(i = 0; i < data.length; i++){
+
+       newTitle = data[i]['title'];
+       newDate = data[i]['date'];
+       newContent = data[i]['content'];
+
+       blogMap.set(i, new Post(newTitle, newDate, newContent));
+       blogMap.get(i);
+
+
+   }
+});
+
+
+
+
 
